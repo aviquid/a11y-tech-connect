ce19398729141e8d5740577f8f5605adef5c6d64
SAPIENT\asark4 <asarkar2@sapient.com>
2018-03-08 22:21:52 +0530

    improved  modal

--- src/Slides/011.js
+++ src/Slides/011.js
@@ -13,6 +13,17 @@ class slide extends React.Component {
       overlay: false
     }
   }
+  handleModalOpen = () => {
+    this.setState({ overlay: true }, function(){
+      document.querySelector("#overlayClose").focus()
+    });
+    
+  }
+  handleModalClose = () => {
+    this.setState({ overlay: false }, function(){
+      document.querySelector(".open-modal-button").focus()
+    })
+  }
   handleTrapTabKey() {
     document.querySelector("#overlayClose").focus();
   }
@@ -23,10 +34,11 @@ class slide extends React.Component {
           <h1>{slide.title}</h1>
           <Step index={1}>
             <h2>Heading</h2>
-            <div className="overlay-container ">
+            <button className="open-modal-button" onClick={this.handleModalOpen} aria-label="Open overlay modal">Open Modal</button>
+            <div className={`overlay-container ${this.state.overlay ? "show" : "hide"}`}>
               <div className="overlay-modal">
                 <div className="overlay-wrapper">
-                  <button id="overlayClose" className="modal-close" aria-label="Close modal">
+                  <button id="overlayClose" className="modal-close" aria-label="Close modal" onClick={this.handleModalClose}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><g fill="none" fillRule="evenodd" stroke="#000" strokeLinecap="square"><path d="M.665.833l18.82 18.821M.665 19.654L19.485.834"></path></g></svg>
                   </button>
                   <h2 className="heading">I'm a accessible Modal</h2>
@@ -34,7 +46,7 @@ class slide extends React.Component {
                     <ul>
                       <li>
                         <label className="sample-input-label" htmlFor="sampleInput">Sample Input</label>
-                        <input className="sampleInput" id="sampleInput" type="text"/>
+                        <input className="sampleInput" id="sampleInput" type="text" />
                       </li>
                       <li>
                         <span className="sample-input-label">Sample Anchor</span>
