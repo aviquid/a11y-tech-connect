935567b5909e7983e86ab564385678fa1e4f6d76
SAPIENT\asark4 <asarkar2@sapient.com>
2018-03-08 21:12:48 +0530

    added modal and colorList

---
+++ src/Slides/011.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { ContentSlide, Step } from 'react-presents';
+import styled from 'styled-components';
+
+const Container = styled.article`
+  display: flex;
+  flex-direction: column;
+`;
+class slide extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      overlay: false
+    }
+  }
+  handleTrapTabKey() {
+    document.querySelector("#overlayClose").focus();
+  }
+  render() {
+    return (
+      <ContentSlide>
+        <Container>
+          <h1>{slide.title}</h1>
+          <Step index={1}>
+            <h2>Heading</h2>
+            <div className="overlay-container ">
+              <div className="overlay-modal">
+                <div className="overlay-wrapper">
+                  <button id="overlayClose" className="modal-close" aria-label="Close modal">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><g fill="none" fillRule="evenodd" stroke="#000" strokeLinecap="square"><path d="M.665.833l18.82 18.821M.665 19.654L19.485.834"></path></g></svg>
+                  </button>
+                  <h2 className="heading">I'm a accessible Modal</h2>
+                  <div className="sub-heading">
+                    <ul>
+                      <li>
+                        <label className="sample-input-label" htmlFor="sampleInput">Sample Input</label>
+                        <input className="sampleInput" id="sampleInput" type="text"/>
+                      </li>
+                      <li>
+                        <span className="sample-input-label">Sample Anchor</span>
+                        <a href="https://webaim.org/" target="_blank">Click Here</a>
+                      </li>
+                    </ul>
+                    <p></p>
+                  </div>
+                  <span tabIndex="0" onFocus={this.handleTrapTabKey}></span>
+                </div>
+              </div>
+            </div>
+          </Step>
+        </Container>
+      </ContentSlide>
+    )
+  }
+}
+
+
+slide.title = 'Modal or Overlay';
+
+export default slide;
