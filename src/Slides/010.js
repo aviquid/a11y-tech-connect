abcde78f61d4b34d67cf0ebafb3c714dab29a20c
SAPIENT\asark4 <asarkar2@sapient.com>
2018-03-09 12:37:02 +0530

    Updated slides

--- src/Slides/010.js
+++ src/Slides/010.js
@@ -2,82 +2,59 @@ import React from 'react';
 import { ContentSlide, Step } from 'react-presents';
 import styled from 'styled-components';
 
+import TitleImage from '../images/why.jpg';
+import mouse from '../images/mouse-stick.jpg';
+import googleSearch from '../images/google-search.PNG';
+
 const Container = styled.article`
   display: flex;
   flex-direction: column;
 `;
-const colorSwatches = {
-  label: "colors",
-  listElement: [{
-    value: "Gold",
-    bgColor: "rgb(207, 190, 169)"
-  }, {
-    value: "Silver",
-    bgColor: "rgb(215, 215, 215)"
-  }, {
-    value: "Gray",
-    bgColor: "rgb(102, 102, 102)"
-  }, {
-    value: "Space Gray",
-    bgColor: "rgb(128, 127, 132)"
-  }]
-}
-const colorSwatches2 = {
-  label: "colors2",
-  listElement: [{
-    value: "red",
-    bgColor: "red"
-  }, {
-    value: "green",
-    bgColor: "green"
-  }, {
-    value: "blue",
-    bgColor: "blue"
-  }, {
-    value: "yellow",
-    bgColor: "yellow"
-  }]
-}
 const slide = () => (
   <ContentSlide>
     <Container className="textAlign-center semantic-tags">
       <h1>{slide.title}</h1>
-      <Step index={1}>
-        <h2>Use the tag for its rightfully purpose</h2>
-      </Step>
-      <Step index={2}>
-        <ul className="colorList">
-          {colorSwatches.listElement.map((value, index) => {
-            return (
-              <li key={index}>
-                <input type="radio" name="colors" value={value.value} id={colorSwatches.label + index} className="colorList-input" />
-                <div className="colorList-dot" style={{ "backgroundColor": value.bgColor }}></div>
-                <label htmlFor={colorSwatches.label + index} className="colorList-label">{value.value}</label>
-              </li>
-            )
-          })}
-        </ul>
-      </Step>
-      <Step index={3}>
-        <h2>Ensure meaning is not conveyed by color alone</h2>
-      </Step>
-      <Step index={3}>
-        <ul className="colorList">
-          {colorSwatches2.listElement.map((value, index) => {
-            return (
-              <li key={index}>
-                <input type="radio" name="colors" value={value.value} id={colorSwatches2.label + index} className="colorList-input hide" />
-                <div className="colorList-dot" style={{ "backgroundColor": value.bgColor }}></div>
-                <label htmlFor={colorSwatches2.label + index} className="colorList-label noText">{value.value}</label>
-              </li>
-            )
-          })}
-        </ul>
-      </Step>
+      <div className="sub-heading-benefits">
+        <Step index={1}>
+          <h2>1. Chrome Extension </h2>
+          <ul>
+            <li>NoCoffee</li>
+            <li>aXe-Coconut</li>
+          </ul>
+        </Step>
+        <Step index={2}>
+          <h2>2. Proper semantic</h2>
+          <ul className="meme-why-img">
+            <li>div div div all the way</li>
+            <li>USe aria for exta info</li>
+            <li><img src={TitleImage} alt="asdsd" /></li>
+          </ul>
+        </Step>
+        <Step index={3}>
+          <h2>3. Proper labeling </h2>
+        </Step>
+        <Step index={4}>
+          <ul className="google-search">
+            <li><img src={googleSearch} alt="" /> </li>
+          </ul>
+        </Step>
+        <Step index={5}>
+          <h2>4. Chrome audit </h2>
+          <ul className="meme-why-img">
+            <li>Accessibility audit</li>
+          </ul>
+        </Step>
+        <Step index={6}>
+          <h2>5. Font-size </h2>
+          <ul className="meme-why-img">
+            <li>Google Settings - Appearance</li>
+          </ul>
+        </Step>
+      </div>
     </Container>
   </ContentSlide>
 );
 
-slide.title = 'Using semantic tags';
+slide.title = 'Good to know';
 
 export default slide;
--- src/Slides/011.js
+++ src/Slides/011.js
@@ -6,67 +6,37 @@ const Container = styled.article`
   display: flex;
   flex-direction: column;
 `;
-class slide extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      overlay: false
-    }
-  }
-  handleModalOpen = () => {
-    this.setState({ overlay: true }, function(){
-      document.querySelector("#overlayClose").focus()
-    });
-    
-  }
-  handleModalClose = () => {
-    this.setState({ overlay: false }, function(){
-      document.querySelector(".open-modal-button").focus()
-    })
-  }
-  handleTrapTabKey() {
-    document.querySelector("#overlayClose").focus();
-  }
-  render() {
-    return (
-      <ContentSlide>
-        <Container className="textAlign-center modal-or-overlay">
-          <h1>{slide.title}</h1>
-          <Step index={1}>
-            <h2>Heading</h2>
-            <button className="open-modal-button" onClick={this.handleModalOpen} aria-label="Open overlay modal">Open Modal</button>
-            <div className={`overlay-container ${this.state.overlay ? "show" : "hide"}`}>
-              <div className="overlay-modal">
-                <div className="overlay-wrapper">
-                  <button id="overlayClose" className="modal-close" aria-label="Close modal" onClick={this.handleModalClose}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><g fill="none" fillRule="evenodd" stroke="#000" strokeLinecap="square"><path d="M.665.833l18.82 18.821M.665 19.654L19.485.834"></path></g></svg>
-                  </button>
-                  <h2 className="heading">I'm a accessible Modal</h2>
-                  <div className="sub-heading">
-                    <ul>
-                      <li>
-                        <label className="sample-input-label" htmlFor="sampleInput">Sample Input</label>
-                        <input className="sampleInput" id="sampleInput" type="text" />
-                      </li>
-                      <li>
-                        <span className="sample-input-label">Sample Anchor</span>
-                        <a href="https://webaim.org/" target="_blank">Click Here</a>
-                      </li>
-                    </ul>
-                    <p></p>
-                  </div>
-                  <span tabIndex="0" onFocus={this.handleTrapTabKey}></span>
-                </div>
-              </div>
-            </div>
-          </Step>
-        </Container>
-      </ContentSlide>
-    )
-  }
-}
+const slide = () => (
+  <ContentSlide>
+    <Container className="textAlign-center semantic-tags">
+      <h1>{slide.title}</h1>
+      <div className="sub-heading-benefits">
+        <Step index={1}>
+          <h2>1. Good for SEO </h2>
+          <ul>
+            <li>Use only one h1 tag in a entire page</li>
+          </ul>
+        </Step>
+        <Step index={2}>
+          <h2>2. Translation tool</h2>
+          <ul>
+            <li>Language  attribute</li>
+          </ul>
+        </Step>
+        <Step index={3}>
+          <h2>3. Screen reader </h2>
+          <ul>
+            <li>NVDA </li>
+            <li>Apple VoiceOver </li>
+            <li>WebAnywhere  </li>
+            <li><strong>Chromevox </strong></li>
+          </ul>
+        </Step>
+      </div>
+    </Container>
+  </ContentSlide>
+);
 
-
-slide.title = 'Modal or Overlay';
+slide.title = 'Few more benefits';
 
 export default slide;
